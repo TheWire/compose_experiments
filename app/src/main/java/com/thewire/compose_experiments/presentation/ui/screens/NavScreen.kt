9876633a1b0e45159a87df521548956b3adcb9d6
TheWire <TheWire@users.noreply.github.com>
2022-02-28 01:06:22 +0000

    ModalDrawer test

--- app/src/main/java/com/thewire/compose_experiments/presentation/ui/screens/NavScreen.kt
+++ app/src/main/java/com/thewire/compose_experiments/presentation/ui/screens/NavScreen.kt
@@ -1,65 +1,85 @@
 package com.thewire.compose_experiments.presentation.ui.screens
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.navigation.NavController
+import kotlinx.coroutines.launch
 
 @Composable
 fun NavScreen(
     navController: NavController
 ) {
-    Column() {
-        Button(
-            onClick = { navController.navigate("TEST_ROUTE_1")}
-        ) {
-            Text("To Screen 1")
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    ModalDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            Text("This is the drawer")
         }
-        Button(
-            onClick = { navController.navigate("TEST_ROUTE_2")}
-        ) {
-            Text("To Screen 2")
-        }
-        Button(
-            onClick = { navController.navigate("TEST_ROUTE_3")}
-        ) {
-            Text("To Screen 3")
-        }
-        Button(
-            onClick = { navController.navigate("TEST_ROUTE_4")}
-        ) {
-            Text("To Screen 4")
-        }
-        Button(
-            onClick = { navController.navigate("TEST_ROUTE_5")}
-        ) {
-            Text("To Screen 5")
-        }
-        Button(
-            onClick = { navController.navigate("TEST_ROUTE_6")}
-        ) {
-            Text("To Screen 6")
-        }
-        Button(
-            onClick = { navController.navigate("TEST_ROUTE_7")}
-        ) {
-            Text("To Screen 7")
-        }
-        Button(
-            onClick = { navController.navigate("TEST_ROUTE_8")}
-        ) {
-            Text("To Screen 8")
-        }
-        Button(
-            onClick = { navController.navigate("TEST_ROUTE_9")}
-        ) {
-            Text("To Screen 9")
-        }
-        Button(
-            onClick = { navController.navigate("TEST_ROUTE_10")}
-        ) {
-            Text("To Screen 10")
+    ) {
+        Column() {
+            Button(
+                onClick = { navController.navigate("TEST_ROUTE_1") }
+            ) {
+                Text("To Screen 1")
+            }
+            Button(
+                onClick = { navController.navigate("TEST_ROUTE_2") }
+            ) {
+                Text("To Screen 2")
+            }
+            Button(
+                onClick = { navController.navigate("TEST_ROUTE_3") }
+            ) {
+                Text("To Screen 3")
+            }
+            Button(
+                onClick = { navController.navigate("TEST_ROUTE_4") }
+            ) {
+                Text("To Screen 4")
+            }
+            Button(
+                onClick = { navController.navigate("TEST_ROUTE_5") }
+            ) {
+                Text("To Screen 5")
+            }
+            Button(
+                onClick = { navController.navigate("TEST_ROUTE_6") }
+            ) {
+                Text("To Screen 6")
+            }
+            Button(
+                onClick = { navController.navigate("TEST_ROUTE_7") }
+            ) {
+                Text("To Screen 7")
+            }
+            Button(
+                onClick = { navController.navigate("TEST_ROUTE_8") }
+            ) {
+                Text("To Screen 8")
+            }
+            Button(
+                onClick = { navController.navigate("TEST_ROUTE_9") }
+            ) {
+                Text("To Screen 9")
+            }
+            Button(
+                onClick = { navController.navigate("TEST_ROUTE_10") }
+            ) {
+                Text("To Screen 10")
+            }
+            Button(
+                onClick = {
+                    scope.launch {
+                        drawerState.open()
+                    }
+                }
+            ) {
+                Text("Open Drawer")
+            }
         }
     }
 }
