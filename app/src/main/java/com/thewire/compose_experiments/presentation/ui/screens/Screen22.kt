e363b28656d8d698e96e516ea025690294db1513
TheWire <TheWire@users.noreply.github.com>
2023-01-23 02:25:47 +0000

    external library test does not compile

--- app/src/main/java/com/thewire/compose_experiments/presentation/ui/screens/Screen22.kt
+++ app/src/main/java/com/thewire/compose_experiments/presentation/ui/screens/Screen22.kt
@@ -5,12 +5,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import com.thewire.experimentallibrary.ExperimentalMath
 
 @Composable
 fun Screen22() {
@@ -28,6 +30,7 @@ fun Screen22() {
         }
         TestButton(enabled= enabled.value, someText)
         Text(someText.value)
+        Cuber()
     }
 }
 
@@ -37,5 +40,22 @@ fun TestButton(enabled: Boolean, someText: MutableState<String>) {
         onClick = {someText.value = "some text"}
     ) {
         Text(if(enabled) "enabled" else "disabled")
+    }
+}
+
+@Composable
+fun Cuber() {
+    Column() {
+        val textState = remember { mutableStateOf("") }
+        TextField(
+            value = textState.value,
+            onValueChange = { textState.value = it }
+        )
+        val experimentalMath = ExperimentalMath()
+        Button(
+            onClick = { textState.value = experimentalMath.cube(textState.value.toFloat()).toString()}
+        ) {
+            Text("cube")
+        }
     }
 }
