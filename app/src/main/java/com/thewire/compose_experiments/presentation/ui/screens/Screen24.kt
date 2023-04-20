86277582f3750e936c265805edef3a82d12cb691
TheWire <TheWire@users.noreply.github.com>
2023-04-20 23:48:17 +0100

    vertical pager

--- app/src/main/java/com/thewire/compose_experiments/presentation/ui/screens/Screen24.kt
+++ app/src/main/java/com/thewire/compose_experiments/presentation/ui/screens/Screen24.kt
@@ -1,11 +1,13 @@
 package com.thewire.compose_experiments.presentation.ui.screens
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -23,14 +25,31 @@ fun Screen24() {
         val pagerState = rememberPagerState()
         HorizontalPager(
             modifier = Modifier
-                .fillMaxSize()
+                .fillMaxWidth()
                 .border(1.dp, Color.Magenta),
             pageCount = 10,
             state = pagerState
         ) { page ->
             Text(
                 text = "Page: $page",
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(color = Color.Green)
+            )
+        }
+        val pagerState2 = rememberPagerState()
+        VerticalPager(
+            modifier = Modifier
+                .fillMaxWidth()
+                .border(1.dp, Color.DarkGray),
+            pageCount = 10,
+            state = pagerState2,
+        ) { page ->
+            Text(
+                text = "Page: $page",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(color = Color.Yellow)
             )
         }
     }
