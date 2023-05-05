e127e9c137969a1252ffdfcbeff2bba06f336734
TheWire <TheWire@users.noreply.github.com>
2023-05-05 23:56:05 +0100

    compose update LazyVerticalGrid Experiment continued

--- app/src/main/java/com/thewire/compose_experiments/presentation/ui/screens/Screen25.kt
+++ app/src/main/java/com/thewire/compose_experiments/presentation/ui/screens/Screen25.kt
@@ -4,17 +4,20 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Card
 import androidx.compose.material.Divider
 import androidx.compose.material.Icon
@@ -32,6 +35,8 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
@@ -39,12 +44,13 @@ import com.thewire.compose_experiments.R
 
 @Composable
 fun Screen25() {
-    Column(modifier =
-    Modifier
-        .fillMaxSize()
-        .verticalScroll(rememberScrollState())
-        .padding(8.dp),
-            verticalArrangement = Arrangement.spacedBy(48.dp)
+    Column(
+        modifier =
+        Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(8.dp),
+        verticalArrangement = Arrangement.spacedBy(48.dp)
     ) {
         WebCard()
         AltCard()
@@ -53,13 +59,38 @@ fun Screen25() {
     }
 }
 
+@Preview
+@Composable
+fun Preview() {
+    FilterGrid()
+}
+
 @Composable
 fun Chip(text: String) {
     Button(
+        modifier = Modifier
+            .height(32.dp),
         onClick = {},
+        colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.Gray),
+        contentPadding = PaddingValues(8.dp, 8.dp, 16.dp, 8.dp)
     ) {
-        Icon(Icons.Default.Check, contentDescription = "menu")
-        Text(text)
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Icon(
+                Icons.Default.Check,
+                modifier = Modifier.size(18.dp),
+                contentDescription = "menu"
+            )
+            Text(
+                text = text,
+                fontFamily = FontFamily(Font(R.font.roboto_medium)),
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Medium,
+                lineHeight = 20.sp
+            )
+        }
     }
 }
 
@@ -118,12 +149,13 @@ fun AltCard() {
                     .padding(8.dp),
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
-              Text("possit scripserit esse phasellus lorem appetere conubia natum atqui eripuit " +
-                        "alienum inani fames finibus etiam eirmod offendit explicari dicat est " +
-                        "platonem dolorem dolore postulant",
-                        fontSize = 14.sp,
-              )
-               Button(onClick = {}) {
+                Text(
+                    "possit scripserit esse phasellus lorem appetere conubia natum atqui eripuit " +
+                            "alienum inani fames finibus etiam eirmod offendit explicari dicat est " +
+                            "platonem dolorem dolore postulant",
+                    fontSize = 14.sp,
+                )
+                Button(onClick = {}) {
                     Text("Read More")
                 }
             }
@@ -163,12 +195,21 @@ fun WebCard() {
                 modifier = Modifier.fillMaxWidth(),
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
-                Column(modifier = Modifier
-                    .fillMaxWidth(),
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth(),
                     verticalArrangement = Arrangement.spacedBy(2.dp)
                 ) {
-                    Text("Founder And Thinker", fontSize = 18.sp, fontFamily = FontFamily(Font(R.font.roboto_regular)))
-                    Text("Jack Mane", fontSize = 18.sp, fontFamily = FontFamily(Font(R.font.roboto_bold)))
+                    Text(
+                        "Founder And Thinker",
+                        fontSize = 18.sp,
+                        fontFamily = FontFamily(Font(R.font.roboto_regular))
+                    )
+                    Text(
+                        "Jack Mane",
+                        fontSize = 18.sp,
+                        fontFamily = FontFamily(Font(R.font.roboto_bold))
+                    )
                 }
                 Row(
                     modifier = Modifier.padding(8.dp),
